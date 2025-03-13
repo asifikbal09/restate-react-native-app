92aae75f2adf25a6bbd848f884ceb9212f6f83cc
asifikbal09 <asif.ikbal662@gmail.com>
2025-03-13 23:32:13 +0600

    [DONE] Home page header design complete.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,24 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="mb-10 text-2xl font-rubik-semibold p-5 rounded-full">Welcome to our restate app</Text>
-      <Link href={"/sign-in"}>SignIn</Link>
-      <Link href={"/explore"}>Explore</Link>
-      <Link href={"/profile"}>Profile</Link>
-      <Link href={"/properties/2"}>Property</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full"> 
+      <View className="px-5">
+        <View className="flex flex-row item-center justify-between mt-5">
+          <View className="flex flex-row item-center">
+            <Image source={images.avatar} className="size-12 rounded-full"/>
+            <View className="flex flex-col justify-center item-start ml-2">
+              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+              <Text className="text-base font-rubik-medium text-black-300">Asif Ikbal</Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6"></Image>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
